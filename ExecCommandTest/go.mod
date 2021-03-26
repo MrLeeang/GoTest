3e22ee9494f04c372dee4481d5b50f3ae92eeadb
li hongwei <lihongwei549@163.com>
2021-03-26 21:23:50 +0800

    up

---
+++ ExecCommandTest/go.mod
@@ -0,0 +1,3 @@
+module GoTest/ExecCommandTest
+
+go 1.14
---
+++ ExecCommandTest/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func execCMD(name string, arg ...string) (bool, string) {
+
+	var code = true
+
+	cmd := exec.Command(name, arg...)
+
+	whoami, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err)
+		code = false
+	}
+
+	return code, string(whoami)
+}
+
+func main() {
+	code, ret := execCMD("sc.exe", "query", "RemoteAccess")
+	if code != true {
+		fmt.Println(ret)
+		return
+	}
+	fmt.Println(ret)
+	// s := string([]rune(ret))
+
+	// countSplit := strings.Split(s, "\n")
+	// fmt.Println(countSplit)
+}
